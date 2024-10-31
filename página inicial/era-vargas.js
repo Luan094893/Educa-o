18f9a47bb3424dc489c9c2dda2b9f8d9b0a51aca
Luan094893 <luanb0673@gmail.com>
2024-10-31 12:14:20 -0300

    Adicionei um pouco do governo proviório
    Apenas a pagina

---
+++ página inicial/era-vargas.js
@@ -0,0 +1,15 @@
+const governo_provisorio = document.getElementById('img1');
+const governo_constitucionalista = document.getElementById('img2');
+const estado_novo = document.getElementById('img13');
+
+governo_provisorio.addEventListener('click', () => {
+    //Lógica para trocar a página;
+});
+
+governo_constitucionalista.addEventListener('click', () => {
+    //Lógica para trocar a página;
+});
+
+estado_novo.addEventListener('click', () => {
+    //Lógica para trocar a página;
+});
